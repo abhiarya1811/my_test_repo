d54fc39023a9378aefa073d8be970ea8f534e316
abhi <aryaabhi1811@gmail.com>
2024-09-18 16:34:43 +0530

    code modified

--- gui_pay.py
+++ gui_pay.py
@@ -19,10 +19,10 @@ window = tk.Tk()
 window.title("Employee Pay Calculator")
 
 hours_label = tk.Label(window, text="Enter number of worked hours:")
-hours_label.grid(row=0, column=0, padx=10, pady=10)
+hours_label.grid(row=0, column=0, padx=100, pady=100)
 
 hours_entry = tk.Entry(window)
-hours_entry.grid(row=0, column=1, padx=10, pady=10)
+hours_entry.grid(row=0, column=1, padx=100, pady=10)
 
 calculate_button = tk.Button(window, text="Calculate Pay", command=calculate_pay)
 calculate_button.grid(row=1, column=0, columnspan=2, pady=20)
